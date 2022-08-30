6f1a1d333a2583ebe7815fb91a2e8b6edb01f3d1
fancn21th <fancn21th@gmail.com>
2022-08-30 15:13:38 +0800

    proxy

--- src/hooks/useSample/utils/format.js
+++ src/hooks/useSample/utils/format.js
@@ -1,18 +1,6 @@
 const format1 = (origin) => {
-  const validLevelList = ["地市级", "省级", "区县级"];
-
-  const a = origin["gsl_zz_info"].reduce((acc, cur) => {
-    if (validLevelList.includes(cur.ADMINI_LEVEL)) {
-      acc += cur.GSL_NUM;
-    }
-    return acc;
-  }, 0);
-
-  return {
-    a,
-    b: "hello",
-    c: "hi",
-  };
+  debugger;
+  return origin.data.result;
 };
 
 export { format1 };
---
+++ src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: process.env.REACT_APP_BASE_URL,
+      changeOrigin: true,
+      // it is really sad that i can not find the config
+      // neither from 'https://create-react-app.dev/docs/proxying-api-requests-in-development#configuring-the-proxy-manually'
+      // nor https://github.com/chimurai/http-proxy-middleware#http-proxy-options
+      logLevel: "debug", // however it works anyway
+      pathRewrite: { "^/api": "" },
+    })
+  );
+};
