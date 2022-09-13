3c0319524c51f6bbba5241ab03a8329901ea60c2
fancn21th <fancn21th@gmail.com>
2022-09-13 17:40:14 +0800

    update useQuery sample

--- src/hooks/useSample/utils/api.js
+++ src/hooks/useSample/utils/api.js
@@ -3,8 +3,10 @@ import { format1 } from "./format";
 
 const fetchSample = () => {
   return new Promise((resolve, reject) => {
-    axiosInstance.get("/api/sample").then((result) => {
-      console.log("result", result);
+    axiosInstance.get("/api/users").then((result) => {
+      console.log({
+        result,
+      });
       resolve(format1(result));
     });
   });
