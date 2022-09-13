a83b390ec0d1d186148e5a4c04ace992f95dd778
fancn21th <fancn21th@gmail.com>
2022-09-13 16:28:15 +0800

    move CapsuleTabs to header

--- src/pages/FooPage/index.js
+++ src/pages/FooPage/index.js
@@ -1,25 +1,29 @@
-import Canvas from "@antv/f2-react";
-import { Chart, Interval, Legend, Axis, Tooltip } from "@antv/f2";
-
-// F2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
-const data = [
-  { genre: "Sports", sold: 275 },
-  { genre: "Strategy", sold: 115 },
-  { genre: "Action", sold: 120 },
-  { genre: "Shooter", sold: 350 },
-  { genre: "Other", sold: 150 },
-];
+import { Grid } from "antd-mobile";
+import Demo from "./components/Demo";
 
 export default function Foo() {
   return (
-    <Canvas pixelRatio={window.devicePixelRatio}>
-      <Chart data={data}>
-        <Legend />
-        <Axis field="genre" />
-        <Axis field="sold" />
-        <Tooltip showTooltipMarker={true} />
-        <Interval x="genre" y="sold" />
-      </Chart>
-    </Canvas>
+    <>
+      <Grid.Item>
+        <div>
+          <Demo />
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div>
+          <Demo />
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div>
+          <Demo />
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div>
+          <Demo />
+        </div>
+      </Grid.Item>
+    </>
   );
 }
