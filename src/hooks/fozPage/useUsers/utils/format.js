d8a6a43706305031f1a8583611007cdc04ac3d63
fancn21th <fancn21th@gmail.com>
2022-09-23 11:43:10 +0800

    remove paged hooks

--- src/hooks/fozPage/useUsers/utils/format.js
+++
@@ -1,7 +0,0 @@
-const format1 = (origin) => {
-  const result = origin.data.result.result;
-  const resultList = Object.keys(result).map((key) => result[key]);
-  return resultList;
-};
-
-export { format1 };
--- src/hooks/fozPage/useUsers/utils/initialData.js
+++
@@ -1,3 +0,0 @@
-const initialUsers = [];
-
-export default initialUsers;
