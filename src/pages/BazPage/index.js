63d5bff64b8f7d17ffec1ee1931433406574fbfa
fancn21th <fancn21th@gmail.com>
2022-09-26 17:42:49 +0800

    ResponsiveContainerColumn

--- src/pages/BazPage/index.js
+++ src/pages/BazPage/index.js
@@ -1,8 +1,36 @@
 import Hello from "../../components/Hello";
 import Basic from "../../components/Basic";
 import PieWithLabel from "../../components/PieWithLabel";
-import ResponsiveContainerColumn from "../../layouts/ResponsiveContainerColumn";
+
+import ResponsiveContainerColumn, {
+  MobileLayout,
+  TabletLayout,
+  Column,
+} from "../../layouts/ResponsiveContainerColumn";
 
 export default function Bar() {
-  return <ResponsiveContainerColumn></ResponsiveContainerColumn>;
+  return (
+    <ResponsiveContainerColumn>
+      <MobileLayout>
+        <Column>
+          <Hello />
+        </Column>
+      </MobileLayout>
+      <TabletLayout>
+        <Column>
+          <Hello />
+          <Basic />
+          <PieWithLabel />
+        </Column>
+        <Column>
+          <PieWithLabel />
+          <Basic />
+          <Hello />
+        </Column>
+        <Column>
+          <PieWithLabel />
+        </Column>
+      </TabletLayout>
+    </ResponsiveContainerColumn>
+  );
 }
